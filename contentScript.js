6808e2de57b51b80527881060ccec2bc6a6d56b9
shamith09 <58998800+shamith09@users.noreply.github.com>
2020-01-24 08:49:50 -0800

    Added creators' names

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-// Created by Shamith Pasula, Bhavya Gupta, Kush Goswami, Archit Pimple
+// Created by Shamith Pasula, Bhavya Gupta, Kush Goswami, Archit Pimple 
 
 const ASSIGNMENT_TABLE_SELECTOR = "#container_content > div > table:nth-child(6) > tbody > tr > td.home_left > table > tbody";
 const ADD_ASSIGNMENT_SELECTOR = '#container_content > div > div.content_spacing_sm';
